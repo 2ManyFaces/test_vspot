f67eb07ff567ff3c9e7c75731e8a2b68ee6d7067
2ManyFaces <tarekalambhuiyan@gmail.com>
2026-04-27 14:32:13 +0600

    docker update

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -15,6 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(append: [
             \App\Http\Middleware\CheckAccountStatus::class,
         ]);
+
+        $middleware->use([
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
